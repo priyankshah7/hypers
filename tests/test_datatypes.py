55f3dbade71cb3b6ba53d6a6ec30a8ce4221d302
Priyank Shah <priyank.shah@kcl.ac.uk>
2018-05-09 14:18:59 +0100

    Updated tests to incorporate Process object model.

--- tests/test_datatypes.py
+++ tests/test_datatypes.py
@@ -1,8 +1,7 @@
 import pytest
 import numpy as np
 
-from skhyper.cluster import KMeans, AgglomerativeClustering, SpectralClustering
-from skhyper.decomposition import PCA
+from skhyper.process import Process
 
 
 class TestDataTypes:
@@ -11,28 +10,11 @@ class TestDataTypes:
         data_1d = np.random.rand(1024)
         data_2d = np.random.rand(20, 1024)
 
-        # clustering techniques
-        mdl_kmeans = KMeans(2)
-        mdl_agglomerative = AgglomerativeClustering(2)
-        mdl_spectral = SpectralClustering(2)
+        # data type checks
+        with pytest.raises(TypeError): Process(data_empty_list)
+        with pytest.raises(TypeError): Process(data_1d)
+        with pytest.raises(TypeError): Process(data_2d)
 
-        # decomposition techniques
-        mdl_pca = PCA()
-
-        # ensure error occurs with a list instead of a numpy array
-        with pytest.raises(TypeError): mdl_kmeans.fit(data_empty_list)
-        with pytest.raises(TypeError): mdl_agglomerative.fit(data_empty_list)
-        with pytest.raises(TypeError): mdl_spectral.fit(data_empty_list)
-        with pytest.raises(TypeError): mdl_pca.fit(data_empty_list)
-
-        # ensure error occurs with 1-dimensional array
-        with pytest.raises(TypeError): mdl_kmeans.fit(data_1d)
-        with pytest.raises(TypeError): mdl_agglomerative.fit(data_1d)
-        with pytest.raises(TypeError): mdl_spectral.fit(data_1d)
-        with pytest.raises(TypeError): mdl_pca.fit(data_1d)
-
-        # ensure error occurs with 2-dimensional array
-        with pytest.raises(TypeError): mdl_kmeans.fit(data_2d)
-        with pytest.raises(TypeError): mdl_agglomerative.fit(data_2d)
-        with pytest.raises(TypeError): mdl_spectral.fit(data_2d)
-        with pytest.raises(TypeError): mdl_pca.fit(data_2d)
+        # ensuring n_samples > n_features
+        data_sf = np.random.rand(10, 10, 1024)
+        with pytest.raises(TypeError): Process(data_sf)

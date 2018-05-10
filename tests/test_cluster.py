83e6d985e64f0f9ecfec0096438dafefb63365c0
Priyank Shah <priyank.shah@kcl.ac.uk>
2018-05-10 19:03:54 +0100

    testing appveyor

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -1,23 +1,22 @@
-# import pytest
-# import numpy as np
-#
-# from skhyper.process import Process
-# from skhyper.cluster import KMeans
-#
-#
-# class TestCluster:
-#     def test_kmeans(self):
-#         data_3d = np.random.rand(6, 6, 25)
-#         data_4d = np.random.rand(5, 5, 2, 25)
-#
-#         X_3d = Process(data_3d)
-#         X_4d = Process(data_4d)
-#
-#         # ensure that 3- and 4-dimensional data produce non-zero labels and data clusters
-#         mdl_3d = KMeans(n_clusters=2)
-#         mdl_3d.fit(X_3d)
-#         assert mdl_3d.labels_ is not None
-#
-#         mdl_4d = KMeans(n_clusters=2)
-#         mdl_4d.fit(X_4d)
-#         assert mdl_4d.labels_ is not None
+import numpy as np
+
+from skhyper.process import Process
+from skhyper.cluster import KMeans
+
+
+class TestCluster:
+    def test_kmeans(self):
+        data_3d = np.random.rand(6, 6, 25)
+        data_4d = np.random.rand(5, 5, 2, 25)
+
+        X_3d = Process(data_3d)
+        X_4d = Process(data_4d)
+
+        # ensure that 3- and 4-dimensional data produce non-zero labels and data clusters
+        mdl_3d = KMeans(n_clusters=2)
+        mdl_3d.fit(X_3d)
+        assert mdl_3d.labels_ is not None
+
+        mdl_4d = KMeans(n_clusters=2)
+        mdl_4d.fit(X_4d)
+        assert mdl_4d.labels_ is not None

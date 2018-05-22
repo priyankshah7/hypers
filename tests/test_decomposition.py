fb5545e41f9f9c8c32fb23b69f3a431d65a963ae
Priyank Shah <priyank.shah@kcl.ac.uk>
2018-05-22 13:51:39 +0100

    Changed from random dataset to one using sklearn's make_blob

--- tests/test_decomposition.py
+++ tests/test_decomposition.py
@@ -1,26 +1,31 @@
 import pytest
 import numpy as np
+from sklearn.datasets import make_blobs
 
 from skhyper.process import Process
-from skhyper.decomposition import PCA
+from skhyper.decomposition import PCA, NMF, FastICA
 
 
 class TestDecomposition:
+    def setup(self):
+        data_3d, label_3d = make_blobs(n_samples=64, n_features=32, centers=3)
+        data_4d, label_4d = make_blobs(n_samples=128, n_features=32, centers=3)
+
+        data_3d = np.reshape(data_3d, (8, 8, 32))
+        data_4d = np.reshape(data_4d, (8, 8, 2, 32))
+
+        self.X_3d = Process(data_3d)
+        self.X_4d = Process(data_4d)
+
     def test_pca(self):
         mdl = PCA()
         with pytest.raises(AttributeError): mdl.plot_statistics()
-
-        data_3d = np.random.rand(6, 6, 25)
-        data_4d = np.random.rand(5, 5, 2, 25)
-
-        X_3d = Process(data_3d)
-        X_4d = Process(data_4d)
 
         mdl_3d = PCA()
         mdl_4d = PCA()
 
         # 3-dimensional data
-        mdl_3d.fit_transform(X_3d)
+        mdl_3d.fit_transform(self.X_3d)
         assert mdl_3d.image_components_ is not None
         assert mdl_3d.spec_components_ is not None
         assert mdl_3d.explained_variance_ is not None
@@ -36,7 +41,7 @@ class TestDecomposition:
         assert Xd_3d is not None
 
         # 4-dimensional data
-        mdl_4d.fit_transform(X_4d)
+        mdl_4d.fit_transform(self.X_4d)
         assert mdl_4d.image_components_ is not None
         assert mdl_4d.spec_components_ is not None
         assert mdl_4d.explained_variance_ is not None
@@ -51,3 +56,50 @@ class TestDecomposition:
         Xd_4d = mdl_4d.inverse_transform(n_components=10, perform_anscombe=False)
         assert Xd_4d is not None
 
+    def test_nmf(self):
+        # Passing only positive values
+        X_3d = self.X_3d
+        X_4d = self.X_4d
+        self.X_3d.data = np.abs(X_3d.data)
+        self.X_4d.data = np.abs(X_4d.data)
+        self.X_3d.update()
+        self.X_4d.update()
+
+        mdl_3d = NMF(n_components=3)
+        mdl_4d = NMF(n_components=3)
+
+        # 3-dimensional data
+        mdl_3d.fit_transform(self.X_3d)
+        assert mdl_3d.reconstruction_err_ is not None
+        assert mdl_3d.n_iter_ is not None
+
+        Xd_3d = mdl_3d.inverse_transform(n_components=1)
+        assert Xd_3d is not None
+
+        # 4-dimensional data
+        mdl_4d.fit_transform(self.X_4d)
+        assert mdl_4d.reconstruction_err_ is not None
+        assert mdl_4d.n_iter_ is not None
+
+        Xd_4d = mdl_4d.inverse_transform(n_components=1)
+        assert Xd_4d is not None
+
+    def test_fastica(self):
+        mdl_3d = FastICA(n_components=3)
+        mdl_4d = FastICA(n_components=3)
+
+        # 3-dimensional data
+        mdl_3d.fit_transform(self.X_3d)
+        assert mdl_3d.mixing_ is not None
+        assert mdl_3d.n_iter_ is not None
+
+        Xd_3d = mdl_3d.inverse_transform(n_components=1)
+        assert Xd_3d is not None
+
+        # 4-dimensional data
+        mdl_4d.fit_transform(self.X_4d)
+        assert mdl_4d.mixing_ is not None
+        assert mdl_4d.n_iter_ is not None
+
+        Xd_4d = mdl_4d.inverse_transform(n_components=1)
+        assert Xd_4d is not None

02d2ee9443f5a3e7d89a460c4f82da8d40d1d79d
Priyank Shah <priyank.shah@kcl.ac.uk>
2018-05-22 16:00:31 +0100

    Changed to return a list of image/spectra of each component instead of an array.

--- skhyper/decomposition/_pca.py
+++ skhyper/decomposition/_pca.py
@@ -67,9 +67,15 @@ class PCA:
 
     Attributes
     ----------
-    image_components_ : array, shape (x, y, (z), n_components)
+    image_components_ : list
+        Returns a list of image arrays for each component. The size is:
+            - n_components: If the number of components were specified
+            - n_features: If the number of components were not specified
 
-    spec_components_ : array, shape(n_features, n_components)
+    spec_components_ : list
+        Returns a list of spectral arrays for each component. The size is:
+            - n_components: If the number of components were specified
+            - n_features: If the number of components were not specified
 
     mdl : object, PCA() instance
         An instance of the PCA model from scikit-learn used when instantiating PCA() from scikit-hyper
@@ -216,10 +222,26 @@ class PCA:
         self.noise_variance_ = mdl.noise_variance_
 
         if not self.n_components:
-            self.image_components_ = np.reshape(w_matrix, self._X.shape)
+            self.image_components_ = [0] * self._X.shape[-1]
+            self.spec_components_ = [0] * self._X.shape[-1]
+
+            w_matrix_ord = np.reshape(w_matrix, self._X.shape)
+            h_matrix_ord = h_matrix.T
+
+            for comp in range(self._X.shape[-1]):
+                self.image_components_[comp] = np.squeeze(w_matrix_ord[..., comp])
+                self.spec_components_[comp] = h_matrix_ord[..., comp]
+
         else:
-            self.image_components_ = np.reshape(w_matrix, self._X.shape[:-1] + (self.n_components,))
-        self.spec_components_ = h_matrix.T
+            self.image_components_ = [0] * self.n_components
+            self.spec_components_ = [0] * self.n_components
+
+            w_matrix_ord = np.reshape(w_matrix, self._X.shape[:-1] + (self.n_components,))
+            h_matrix_ord = h_matrix.T
+
+            for comp in range(self.n_components):
+                self.image_components_[comp] = np.squeeze(w_matrix_ord[..., comp])
+                self.spec_components_[comp] = h_matrix_ord[..., comp]
 
         return self
 

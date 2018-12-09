1fd1ef85a127eab5df2a2d8eb81f957d959bf703
Priyank Shah <priyank.shah@kcl.ac.uk>
2018-12-09 01:33:01 +0000

    Removing skhyper wrappers for scikit-learn classes

--- skhyper/cluster/__init__.py
+++
@@ -1,3 +0,0 @@
-from skhyper.cluster._kmeans import KMeans
-
-__all__ = ['KMeans']
--- skhyper/decomposition/__init__.py
+++
@@ -1,5 +0,0 @@
-from skhyper.decomposition._pca import PCA
-from skhyper.decomposition._fastica import FastICA
-from skhyper.decomposition._nmf import NMF
-
-__all__ = ['PCA', 'FastICA', 'NMF']

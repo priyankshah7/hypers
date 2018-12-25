9abe5d14b25066ea84b71d290fd09e45ff35d67f
Priyank Shah <priyank.shah@kcl.ac.uk>
2018-12-25 02:40:10 +0000

    Added support for AffinityPropagation, MeanShift, DBSCAN

--- hypers/_learning/_cluster.py
+++ hypers/_learning/_cluster.py
@@ -1,19 +1,33 @@
 import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.cluster import (
-    KMeans, AffinityPropagation, MeanShift, SpectralClustering, AgglomerativeClustering,
-    DBSCAN, Birch
+    KMeans, AffinityPropagation, MeanShift, SpectralClustering, AgglomerativeClustering, DBSCAN
 )
 
 CLUSTER_TYPES = (
     KMeans,
     AffinityPropagation,
-    # MeanShift,
+    MeanShift,
     SpectralClustering,
     AgglomerativeClustering,
-    # DBSCAN,
-    # Birch
+    DBSCAN
 )
+
+
+def _n_clusters(X, mdl):
+    if not type(mdl) in (AffinityPropagation, MeanShift, DBSCAN):
+        n_clusters = X.mdl_cluster.get_params()['n_clusters']
+
+    elif type(mdl) == AffinityPropagation:
+        n_clusters = X.mdl_cluster.cluster_centers_indices_.shape[0]
+
+    elif type(mdl) == MeanShift:
+        n_clusters = X.mdl_cluster.cluster_centers_.shape[0]
+
+    elif type(mdl) == DBSCAN:
+        n_clusters = X.mdl_cluster.core_sample_indices_.shape[0]
+
+    return n_clusters
 
 
 def _data_cluster(X, mdl, decomposed=False, pca_comps=4):
@@ -21,58 +35,42 @@ def _data_cluster(X, mdl, decomposed=False, pca_comps=4):
         raise TypeError('Must pass a sklearn cluster class. Refer to documentation.')
 
     X.mdl_cluster = mdl
-    if not type(mdl) == AffinityPropagation:
-        n_clusters = X.mdl_cluster.get_params()['n_clusters']
-    
+
     if decomposed:
         print('Clustering with the first ' + str(pca_comps) + ' PCA components.')
         mdl_pca = PCA(n_components=pca_comps)
         comps = mdl_pca.fit_transform(X.flatten())
         X.mdl_cluster.fit(comps)
-        if type(X.mdl_cluster) == AffinityPropagation:
-            n_clusters = X.mdl_cluster.cluster_centers_indices.shape[0]
+        n_clusters = _n_clusters(X, mdl)
+        if type(X.mdl_cluster) in (AffinityPropagation, MeanShift):
+            n_clusters = X.mdl_cluster.cluster_centers_.shape[0]
         labels = X.mdl_cluster.labels_.reshape(X.data.shape[:-1])
-            
-        try:
-            specs = mdl_pca.inverse_transform(X.mdl_cluster.cluster_centers_)
-        except AttributeError:
-            specs = np.zeros((n_clusters, X.data.shape[-1]))
-            lbls = labels + 1
-            for cluster_number in range(n_clusters):
-                msk = np.zeros(X.data.shape)
-                for spectral_point in range(X.data.shape[-1]):
-                    msk[..., spectral_point] = np.multiply(
-                        X.data[..., spectral_point], 
-                        np.where(lbls==cluster_number+1, lbls, 0)/(cluster_number+1)
-                    )
-                    
-                if X.ndim == 3:
-                    specs[cluster_number, :] = np.squeeze(np.mean(np.mean(msk, 1), 0))
-                elif X.ndim == 4:
-                    specs[cluster_number, :] = np.squeeze(np.mean(np.mean(np.mean(msk, 2), 1), 0))
 
     else:
         X.mdl_cluster.fit(X.flatten())
-        if type(X.mdl_cluster) == AffinityPropagation:
-            n_clusters = X.mdl_cluster.cluster_centers_indices.shape[0]
+        n_clusters = _n_clusters(X, mdl)
         labels = X.mdl_cluster.labels_.reshape(X.data.shape[:-1])
 
-        try:
+    try:
+        if decomposed:
+            specs = mdl_pca.inverse_transform(X.mdl_cluster.cluster_centers_)
+        else:
             specs = X.mdl_cluster.cluster_centers_
-        except AttributeError:
-            specs = np.zeros((n_clusters, X.data.shape[-1]))
-            lbls = labels + 1
-            for cluster_number in range(n_clusters):
-                msk = np.zeros(X.data.shape)
-                for spectral_point in range(X.data.shape[-1]):
-                    msk[..., spectral_point] = np.multiply(
-                        X.data[..., spectral_point], 
-                        np.where(lbls==cluster_number+1, lbls, 0)/(cluster_number+1)
-                    )
+
+    except AttributeError:
+        specs = np.zeros((n_clusters, X.data.shape[-1]))
+        lbls = labels + 1
+        for cluster_number in range(n_clusters):
+            msk = np.zeros(X.data.shape)
+            for spectral_point in range(X.data.shape[-1]):
+                msk[..., spectral_point] = np.multiply(
+                    X.data[..., spectral_point],
+                    np.where(lbls == cluster_number+1, lbls, 0)/(cluster_number+1)
+                )
                     
-                if X.ndim == 3:
-                    specs[cluster_number, :] = np.squeeze(np.mean(np.mean(msk, 1), 0))
-                elif X.ndim == 4:
-                    specs[cluster_number, :] = np.squeeze(np.mean(np.mean(np.mean(msk, 2), 1), 0))
+            if X.ndim == 3:
+                specs[cluster_number, :] = np.squeeze(np.mean(np.mean(msk, 1), 0))
+            elif X.ndim == 4:
+                specs[cluster_number, :] = np.squeeze(np.mean(np.mean(np.mean(msk, 2), 1), 0))
 
     return labels, specs
